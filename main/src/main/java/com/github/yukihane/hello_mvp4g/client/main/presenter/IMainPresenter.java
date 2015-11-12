0cf5c528f1106c5b81304a000d9a45aed5a21b4e
yukihane <yukihane.feather@gmail.com>
2015-11-12 21:31:28 +0900

    add comment to interfaces

--- main/src/main/java/com/github/yukihane/hello_mvp4g/client/main/presenter/IMainPresenter.java
+++ main/src/main/java/com/github/yukihane/hello_mvp4g/client/main/presenter/IMainPresenter.java
@@ -5,6 +5,9 @@ import com.mvp4g.client.presenter.PresenterInterface;
 
 public interface IMainPresenter extends PresenterInterface<IMainView, MainEventBus> {
 
+    /**
+     * 入力した名前を送信する.
+     */
     void sendNameToServer();
 
 }
--- main/src/main/java/com/github/yukihane/hello_mvp4g/client/main/presenter/IMainView.java
+++ main/src/main/java/com/github/yukihane/hello_mvp4g/client/main/presenter/IMainView.java
@@ -4,22 +4,49 @@ import com.mvp4g.client.view.ReverseViewInterface;
 
 public interface IMainView extends ReverseViewInterface<IMainPresenter> {
 
+    /**
+     * 名前フィールドを選択状態にする.
+     */
     void selectNameField();
 
+    /**
+     * エラー表示をクリアする.
+     */
     void clearError();
 
+    /**
+     * エラーを表示する
+     */
+    void printError(String errorText);
+
+    /**
+     * 送信するテキスト(名前)を取得する.
+     */
     String getTextToServer();
 
-    void printError(String string);
+    /**
+     * 送信するテキストを表示する.
+     */
+    void setTextToServer(String text);
 
+    /**
+     * サーバから受信した応答テキストを表示する.
+     */
+    void setResponseText(String text);
+
+    /**
+     * 送信ボタン押下可能状態を設定する.
+     */
     void setSendButtonEnabled(boolean b);
 
-    void setResponseText(String string);
-
-    void setTextToServer(String textToServer);
-
-    void showErrorDialog();
-
+    /**
+     * 送信成功ダイアログを表示する.
+     */
     void showSuccessDialog(String result);
+
+    /**
+     * 送信エラーダイアログを表示する.
+     */
+    void showErrorDialog();
 
 }
